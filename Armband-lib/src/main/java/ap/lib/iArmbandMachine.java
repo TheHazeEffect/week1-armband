704ae4f8c2e69cf7733a7a242b7cda8f4d962c3b
1602215 <1602215@LABCWS-17.STUDENTS.utech.edu.jm>
2019-01-14 16:31:54 -0500

    Armband Machine interface

---
+++ Armband-lib/src/main/java/ap/lib/iArmbandMachine.java
@@ -0,0 +1,12 @@
+package ap.lib;
+
+public interface iArmbandMachine {
+	/**
+	 * <h1>getCost</h1>
+	 * Calculates the cost of an armband
+	 * @param price - Cost of the armband
+	 * @param quantity - amount of armbands
+	 * @return
+	 */
+	float getCost(float price,int quantity);
+}
